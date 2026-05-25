Added saving of runtime informations, fixed error with FPS calculation, cleanup and small error fixes.

gaussian_splatting/utils/loss_utils.py
@@ -16,10 +16,11 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
+import lpips
 
 
 def l1_loss(network_output, gt):
-    return torch.abs((network_output - gt)).mean()
+    return torch.abs(network_output - gt).mean()
 
 
 def l1_loss_weight(network_output, gt):

slam.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 import time
@@ -130,10 +131,27 @@ class SLAM:
         end.record()
         torch.cuda.synchronize()
         # empty the frontend queue
-        N_frames = len(self.frontend.cameras)
+        # N_frames = len(self.frontend.cameras)
+        N_frames = len(self.frontend.poses)
         FPS = N_frames / (start.elapsed_time(end) * 0.001)
         Log("Total time", start.elapsed_time(end) * 0.001, tag="Eval")
         Log("Total FPS", N_frames / (start.elapsed_time(end) * 0.001), tag="Eval")
+
+        # save final run stats
+        if self.save_dir is not None and self.config['Results']['save_results']:
+            run_stats = {
+                'total_time_sec': start.elapsed_time(end) * 0.001,
+                'total_frames': N_frames,
+                'fps': FPS,
+            }
+
+            mkdir_p(self.save_dir)
+            with open(
+                os.path.join(self.save_dir, 'final_run_stats.json'),
+                'w',
+                encoding='utf-8',
+            ) as f:
+                json.dump(run_stats, f, indent=4)
 
         if self.eval_rendering:
             if N_frames >= 2:

utils/slam_backend.py
@@ -301,7 +301,7 @@ class BackEnd(mp.Process):
                                 )
                             if to_prune is not None:
                                 self.gaussians.prune_points(to_prune.cuda())
-                                for idx in range((len(current_window))):
+                                for idx in range(len(current_window)):
                                     current_idx = current_window[idx]
                                     self.occ_aware_visibility[current_idx] = (
                                         self.occ_aware_visibility[current_idx][~to_prune]
@@ -605,9 +605,10 @@ class BackEnd(mp.Process):
                     indices_to_remove = data[1]
                     for idx in indices_to_remove:
                         if idx in self.viewpoints:
+                            self.viewpoints[idx].clean()
                             del self.viewpoints[idx]
-                            torch.cuda.empty_cache()
-                            gc.collect()
+                    torch.cuda.empty_cache()
+                    gc.collect()
                     self.current_window = [idx for idx in self.current_window if idx not in indices_to_remove]
                 else:
                     raise Exception("Unprocessed data", data)